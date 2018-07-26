a8f6b6f13121e07c78ccb79b41e6bbc116fa85ae
mainephd <jermaine_davis1@hotmail.com>
2018-07-26 16:57:33 -0400

    Add option to source in telegraf environment

--- src/metrics/data/data.go
+++ src/metrics/data/data.go
@@ -1,5 +1,5 @@
 package data
 
 func TelegrafBackgroundScript() string {
-	return "telegraf --config ./telegraf.conf >&1 2>&1 &"
+	return "[[ -f ./telegraf.env ]] && source ./telegraf.env; telegraf --config ./telegraf.conf >&1 2>&1 &"
 }
