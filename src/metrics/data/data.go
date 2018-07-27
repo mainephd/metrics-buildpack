6669234abff62298c16adbb3d0088ac388a3a157
mainephd <jermaine_davis1@hotmail.com>
2018-07-27 08:24:07 -0400

    Make sure environment file is executable

--- src/metrics/data/data.go
+++ src/metrics/data/data.go
@@ -1,5 +1,6 @@
 package data
 
+// TelegrafBackgroundScript Returns a one line bash command to source in a custom environment and start telegraf in the background
 func TelegrafBackgroundScript() string {
-	return "[[ -f ./telegraf.env ]] && source ./telegraf.env; telegraf --config ./telegraf.conf >&1 2>&1 &"
+	return "[[ -f ./telegraf.env ]] && chmod +x ./telegraf.env && source ./telegraf.env; telegraf --config ./telegraf.conf >&1 2>&1 &\n"
 }
